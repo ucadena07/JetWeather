c239f1b52ac15409a92177cf2ce736f6c6e506ec
Ulises Cadena <ucadena07@gmail.com>
2023-12-15 06:07:02 -0600

    creating the main screen widget

--- app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
@@ -1,23 +1,36 @@
 package com.example.jetweatherforecast.screens.main
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.jetweatherforecast.data.DataOrException
 import com.example.jetweatherforecast.model.Weather
 import com.example.jetweatherforecast.widgets.WeatherAppBar
@@ -49,10 +62,44 @@ fun MainScaffold(weather: Weather,navController: NavController) {
 
 @Composable
 fun MainContent(padding: PaddingValues,data: Weather){
-    Column(modifier = Modifier.padding(padding).fillMaxWidth(),
+    var imageUrl = "https://openweathermap.org/img/wn/${data!!.list[0].weather[0].icon}.png"
+    Column(modifier = Modifier
+        .padding(padding)
+        .fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Hello world")
+        Text(text = "Dec 14",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.secondary,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+        Surface(modifier = Modifier
+            .padding(4.dp)
+            .size(200.dp), shape = CircleShape, color = Color(0xFFFFFC400)
+        ) {
+            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(text = "56", style = MaterialTheme.typography.headlineLarge, fontWeight = FontWeight.ExtraBold)
+                Text(text = "Snow",  fontStyle = FontStyle.Italic)
+            }
+        }
     }
+}
 
+@Composable
+fun WeatherStateImage(imageUrl: String){
+//    Image(
+//        painter = rememberAsyncImagePainter(model = imageUrl),
+//        contentDescription = "image",
+//        modifier = Modifier.size(80.dp)
+//    )
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .crossfade(true)
+            .build(),
+        contentDescription = "poster",
+        modifier = Modifier.size(80.dp)
+    )
 }
