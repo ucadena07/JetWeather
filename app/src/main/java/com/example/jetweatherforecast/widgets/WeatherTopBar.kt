12d918b42be4d88d06ae9d7caf7081111fedc5ba
Ulises Cadena <ucadena07@gmail.com>
2023-12-17 09:05:13 -0600

    setting up the dialog

--- app/src/main/java/com/example/jetweatherforecast/widgets/WeatherTopBar.kt
+++ app/src/main/java/com/example/jetweatherforecast/widgets/WeatherTopBar.kt
@@ -1,11 +1,22 @@
 package com.example.jetweatherforecast.widgets
 
 import android.text.BoringLayout
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,7 +26,14 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -38,6 +56,13 @@ fun WeatherAppBar(
     onButtonClicked: () -> Unit = {}
 
 ) {
+    val showDialog = remember{
+        mutableStateOf(false)
+    }
+    if(showDialog.value){
+        ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
+    }
+
     TopAppBar(
         title = {
             Text(
@@ -54,7 +79,7 @@ fun WeatherAppBar(
                       IconButton(onClick = { onAddActionClicked.invoke() }) {
                           Icon(imageVector = Icons.Default.Search, contentDescription = "search")
                       }
-                      IconButton(onClick = { onButtonClicked.invoke() }) {
+                      IconButton(onClick = { showDialog.value = true }) {
                           Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = "more icon")
                       }
                   }else{
@@ -73,4 +98,62 @@ fun WeatherAppBar(
         }
         )
 
+}
+
+@Composable
+fun ShowSettingDropDownMenu(showDialog: MutableState<Boolean>, navController: NavController) {
+    var expanded by remember { mutableStateOf(true) }
+    val items = listOf("About", "Favorites", "Settings")
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .absolutePadding(top = 45.dp, right = 20.dp)
+    ) {
+        DropdownMenu(
+            expanded = expanded, onDismissRequest = { expanded = false
+                showDialog.value = false }, modifier = Modifier
+                .width(140.dp)
+                .background(
+                    Color.White
+                )
+        ) {
+            items.forEachIndexed { index, s ->
+                when (s) {
+                    "About" -> DropdownMenuItem(
+                        text = { Text(text = s) },
+                        onClick = { expanded = false
+                                  showDialog.value = false},
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Info,
+                                contentDescription = ""
+                            )
+                        })
+
+                    "Favorites" -> DropdownMenuItem(
+                        text = { Text(text = s) },
+                        onClick = { expanded = false
+                            showDialog.value = false},
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.FavoriteBorder,
+                                contentDescription = ""
+                            )
+                        })
+                    else -> DropdownMenuItem(
+                        text = { Text(text = s) },
+                        onClick = { expanded = false
+                            showDialog.value = false },
+                        leadingIcon  = {
+                            Icon(
+                                imageVector = Icons.Default.Settings,
+                                contentDescription = ""
+                            )
+                        })
+
+                }
+            }
+        }
+    }
 }
